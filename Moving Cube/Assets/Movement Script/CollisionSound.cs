1f5cce485ccc3e5c84d6cf831e7557414e31bceb
Edward <Brakye>
2019-10-06 18:34:08 -0400

    Final
    Project 1

---
+++ Moving Cube/Assets/Movement Script/CollisionSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour
+{
+
+    public AudioSource tickSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        tickSource = GetComponent<AudioSource>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Target")
+        {
+            tickSource.Play();
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
